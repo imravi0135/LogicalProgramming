fda6f0d93cbf1aaf567d13a2d6e4d0612f5e2fb5
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-15 17:33:07 +0530

    welcome to logical problems

---
+++ LogicalProgramming/LogicalProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class LogicalProgram
+    {
+        public void Fibonacci()
+        {
+            int num1 = 0, num2 = 1, num3, i, num;
+            Console.WriteLine("Enter the number:- ");
+            num = int.Parse(Console.ReadLine());
+            Console.Write(num1 + " " + num2 + " ");
+            for (i = 2; i < num; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
+        }
+    }
+}
+
---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            LogicalProgram logical = new LogicalProgram();
+            logical.Fibonacci();
+            Console.ReadLine();
+            
+        }
+    }
+}
